12de1e58ce6452f685fdaff2d0171a9f5423f860
Geoffroi <geoffroi.henquin@gmail.com>
2021-03-25 22:12:25 -0400

    second

---
+++ src/services/config.js
@@ -0,0 +1,20 @@
+/*
+  Ce fichier contient les variables de configuration
+  reliées à Firebase et ses produits.
+*/
+
+// Configuration Firebase (mettez-y les vôtres !)
+const firebaseConfig = {
+  apiKey: "AIzaSyCDd3wn5EyoL0xkVL9aJJwTJTvVlUdIYso",
+  authDomain: "gh-pvt-hiver-2021.firebaseapp.com",
+  projectId: "gh-pvt-hiver-2021",
+  storageBucket: "gh-pvt-hiver-2021.appspot.com",
+  messagingSenderId: "949483338277",
+  appId: "1:949483338277:web:c1ecd9c9ffb204d446ddcc"
+};
+export default firebaseConfig;
+
+// Nom de la collection principale
+export const utilRef = "utilisateurs-ex5";
+// Nom de la sous-collection
+export const dossRef = "dossiers";
